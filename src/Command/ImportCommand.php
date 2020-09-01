6873414f8a1781808e2e3404d3828e3d6e978f97
Stefan Eroteev <s.eroteev@adcash.com>
2020-09-01 10:11:43 +0300

    Create import command basic structure

---
+++ src/Command/ImportCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Finance\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportCommand extends Command
+{
+    protected static $defaultName = 'import';
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ... put here the code to run in your command
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
+}
